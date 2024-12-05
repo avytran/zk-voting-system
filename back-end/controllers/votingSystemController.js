cacb9b6d5a88fb55cee0f794b7076c89042cbdff
avytran <tr.anhvy.work@gmail.com>
2024-12-06 01:16:47 +0700

    create voting system controller

---
+++ back-end/controllers/votingSystemController.js
@@ -0,0 +1,15 @@
+const candidatesService = require('../models/votingSystemModel'); // Nhập candidateService  
+
+const candidatesController = {  
+    getAllCandidates: async (req, res) => {  
+        try {  
+            const candidates = await candidatesService.getCandidates();  
+            return res.status(200).json(candidates); // Trả về danh sách ứng viên  
+        } catch (error) {  
+            console.error("Đã xảy ra lỗi khi lấy danh sách ứng viên:", error);  
+            return res.status(500).json({ message: "Đã xảy ra lỗi trong quá trình lấy dữ liệu ứng viên." });  
+        }  
+    }  
+};  
+
+module.exports = candidatesController; // Xuất candidateController
